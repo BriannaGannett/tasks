28d1ba138af94d11357aa4c06d54147168d1d87a
Brianna Gannett <brianna.gannett@maine.edu>
2024-10-24 23:16:03 -0400

    Fixed findQuestion function

--- src/nested.ts
+++ src/nested.ts
@@ -35,7 +35,10 @@ export function findQuestion(
     questions: Question[],
     id: number,
 ): Question | null {
-    return null;
+    const newQuestion = questions.find(
+        (question: Question): boolean => question.id === id,
+    );
+    return newQuestion || null;
 }
 
 /**
