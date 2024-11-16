e703d04cfa85416eb31a47adee8b90bb5492aefa
Brianna Gannett <brianna.gannett@maine.edu>
2024-11-15 22:36:27 -0500

    Fixed multiple choice Question componenent

--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function MultipleChoiceQuestion({
     options,
@@ -7,9 +8,21 @@ export function MultipleChoiceQuestion({
     options: string[];
     expectedAnswer: string;
 }): React.JSX.Element {
+    const [choice, setChoice] = useState<string>(options[0]);
+    function selectChoice(event: React.ChangeEvent<HTMLSelectElement>) {
+        setChoice(event.target.value);
+    }
     return (
         <div>
             <h3>Multiple Choice Question</h3>
+            <Form.Select value={choice} onChange={selectChoice}>
+                {options.map((currentOption, index) => (
+                    <option key={index} value={currentOption}>
+                        {currentOption}
+                    </option>
+                ))}
+            </Form.Select>
+            <p>{choice === expectedAnswer ? "✔️" : "❌"}</p>
         </div>
     );
 }
